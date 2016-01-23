b7c1684a37e41c1467c14d2ca76847bcd4e3c217
vermont2050 <domashke@gmail.com>
2016-01-23 20:57:57 +0300

    Ослы

--- domashke_tester/Form1.cs
+++ domashke_tester/Form1.cs
@@ -16,5 +16,10 @@ namespace domashke_tester
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Привет ослы");
+        }
     }
 }
